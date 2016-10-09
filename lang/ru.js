6550fd17b6d8c1b5c1aea69f100e301aea3dd0e2
froelz1337 <frenak12@gmail.com>
2016-10-09 13:42:40 +0300

    Update ru.js

--- lang/ru.js
+++ lang/ru.js
@@ -1,3 +1,26 @@
 module.exports = {
+	uncategorized: {
+		search: "Поиск...",
+		leaderboard: "Рейтинги",
+		login: "Вход",
+		logout: "Выход",
+		register: "Регистрация",
+		performance: "Производительность",
+		score: "Очки",
+		dashboard: "Панель",
+	},
+	auth: {
+		login: {
+			name: "Имя",
+			password: "Пароль",
 
+			forgotpass: "Забыли свои пароль?",
+
+			success: "Вы успешно вошли",
+			error: "Пожалуйста проверьте свои имя и/или пароль"
+		},
+		logout: {
+			success: "Вы успешно вышли"
+		}
+	}
 }
