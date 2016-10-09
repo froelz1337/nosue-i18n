69ba9d8c713eff431584c3394b325b89f1578c13
froelz1337 <frenak12@gmail.com>
2016-10-09 13:21:36 +0300

    Update i18n.js
    russian

--- i18n.js
+++ i18n.js
@@ -52,6 +52,28 @@ module.exports = {
 		}
 	},
 	ru: { // Russian
+		uncategorized: {
+			search: "Поиск...",
+			leaderboard: "Рейтинги",
+			login: "Вход",
+			logout: "Выход",
+			register: "Регистрация",
+			performance: "Производительность",
+			score: "Очки",
+			dashboard: "Панель",
+		},
+		auth: {
+			login: {
+				name: "Имя",
+				password: "Пароль",
+
+				forgotpass: "Забыли свой пароль?",
+
+				success: "Вы успешно вошли",
+				error: "Пожалуйста проверьте свои имя и/или пароль"
+			},
+			logout: {
+				success: "Вы успешно вышли"
 
 	},
 	jp: { // Japanese
